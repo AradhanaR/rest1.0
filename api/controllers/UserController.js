4b2537bfd5999041d47b5d3a3c94cac172dfb25b
aradhana <aradhana.projects@gmail.com>
2019-04-29 20:45:04 +0530

    Added user login action

---
+++ api/controllers/UserController.js
@@ -0,0 +1,19 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  /**
+   * Login action
+   * @param {*} req 
+   * @param {*} res 
+   */
+  login: function (req, res) {
+    sails.log.debug('TODO: implement');
+    return res.ok();
+  }
+};
+
--- config/routes.js
+++ config/routes.js
@@ -32,6 +32,6 @@ module.exports.routes = {
   * not match any of those, it is matched against static assets.             *
   *                                                                          *
   ***************************************************************************/
-
+  'api/v1/login': 'UserController/login'
 
 };
