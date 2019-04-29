87bacbd2a143a6fa5a37bcf1a9449064036d09cc
aradhana <aradhana.projects@gmail.com>
2019-04-29 20:34:00 +0530

    User controller signup

---
+++ api/controllers/UserController.js
@@ -0,0 +1,23 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+  /**
+   * user/signup.js
+   *
+   * Signup user.
+   */
+  signup: function (req, res) {
+
+    sails.log.debug('TODO: implement');
+    return res.ok();
+
+  }
+
+};
+
